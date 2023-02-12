9909e591c8f6e0583f3273bf7bc869921b888306
BaamPark <baam.park.95@gmail.com>
2023-02-11 21:50:31 -0500

    minkinn.py is modified for color change

--- miniknn.py
+++ miniknn.py
@@ -52,7 +52,7 @@ train_y = mini_train[:,1]
 fig = plt.figure()
 plt.scatter(train_x[np.where(mini_train_label==0)], train_y[np.where(mini_train_label==0)], color='red')
 plt.scatter(train_x[np.where(mini_train_label==1)], train_y[np.where(mini_train_label==1)], color='blue')
-plt.scatter(train_x[np.where(mini_train_label==2)], train_y[np.where(mini_train_label==2)], color='yellow')
+plt.scatter(train_x[np.where(mini_train_label==2)], train_y[np.where(mini_train_label==2)], color='orange')
 plt.scatter(train_x[np.where(mini_train_label==3)], train_y[np.where(mini_train_label==3)], color='black')
 
 test_x = mini_test[:,0]
@@ -60,7 +60,7 @@ test_y = mini_test[:,1]
 outputlabels = np.array(outputlabels)
 plt.scatter(test_x[np.where(outputlabels==0)], test_y[np.where(outputlabels==0)], marker='^', color='red')
 plt.scatter(test_x[np.where(outputlabels==1)], test_y[np.where(outputlabels==1)], marker='^', color='blue')
-plt.scatter(test_x[np.where(outputlabels==2)], test_y[np.where(outputlabels==2)], marker='^', color='yellow')
+plt.scatter(test_x[np.where(outputlabels==2)], test_y[np.where(outputlabels==2)], marker='^', color='orange')
 plt.scatter(test_x[np.where(outputlabels==3)], test_y[np.where(outputlabels==3)], marker='^', color='black')
 
 # save diagram as png file
